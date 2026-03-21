c58889ed8ffed9aa943213a62542472344c1538f
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2026-03-21 20:07:19 +0530

    learn backend connection

--- ecommers-website/src/App.jsx
+++ ecommers-website/src/App.jsx
@@ -1,16 +1,28 @@
 import { Routes , Route } from 'react-router'
-
+import { useEffect , useState } from 'react'
 import './App.css'
 import { HomePage } from './pages/HomePage'
 import { Checkout } from './pages/Checkout'
 import { Orders } from './pages/orders'
 
+import  axios from 'axios'
+
 function App() {
+      const [ cart, setCart] = useState([])
+
+      useEffect(()=>{
+        axios.get('/api/cart-items?expand=product')  //query parimeter 
+        .then((response) => {
+            setCart(response.data)
+    })
+      }, [])
+
+
 
   return (
     <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path="checkout" element={ <Checkout/>}/>
+      <Route index element={<HomePage cart = {cart}/>}/>
+      <Route path="checkout" element={ <Checkout cart = {cart}/>}/>
       <Route path="orders" element={ <Orders/>} />
     </Routes>
   )
